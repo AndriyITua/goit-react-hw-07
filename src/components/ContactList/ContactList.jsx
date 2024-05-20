3f32aca56a06ea6264eb24eb7d68f4636f158426
Andriy Zhylchuk <zhylchuk.andriy@gmail.com>
2024-05-20 23:58:25 +0300

    changes in ContactList

--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -2,16 +2,10 @@ import Contact from "../Contact/Contact";
 import css from "../ContactList/ContactList.module.css";
 
 import { useSelector } from "react-redux";
-import { getContacts, getSearchQuery } from "../../redux/selectors";
+import { selectFilteredContacts } from "../../redux/selectors";
 
 export default function ContactList() {
-  const contacts = useSelector(getContacts);
-
-  const valueSearch = useSelector(getSearchQuery);
-
-  const visibleContacts = contacts.items.filter((contact) =>
-    contact.name.toLowerCase().includes(valueSearch.name.toLowerCase())
-  );
+  const visibleContacts = useSelector(selectFilteredContacts);
 
   return (
     <ul className={css.list}>
